e8fc4a1bc80658f5da56d2eb383951229efb8ab4
Andreas Jaeger <aj@suse.com>
2016-06-15 10:12:29 +0200

    Use proper theme for release notes
    Use oslosphinx as theme - watcher is an official project and thus
    should use it.
    
    Change-Id: I0ab1ad3e99a92e6b59a4bcb29f216b4af6cbac6a

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -26,7 +26,8 @@ from watcher import version as watcher_version
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['reno.sphinxext']
+extensions = ['reno.sphinxext',
+              'oslosphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
