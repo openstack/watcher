aef1eba5dff3bc44deac166b078751561f688996
licanwei <li.canwei2@zte.com.cn>
2016-07-19 10:58:14 +0800

    test_context_hook_before_method failed
    gate-watcher-python27 FAILURE
    The reason is that oslo_context.context.__init__()
    added parameter 'domain_name'.
    There are some mismatching in file '/watcher/common/context.py'
    
    Change-Id: If0be05943e7c89788d6ccba3385474ccb036e6f5
    Closes-Bug: #1604214

--- watcher/common/context.py
+++ watcher/common/context.py
@@ -29,6 +29,14 @@ class RequestContext(context.RequestContext):
                               without authentication.
 
         """
+        super(RequestContext, self).__init__(auth_token=auth_token,
+                                             user=user, tenant=project,
+                                             is_admin=is_admin,
+                                             read_only=read_only,
+                                             show_deleted=show_deleted,
+                                             request_id=request_id,
+                                             roles=roles)
+
         self.is_public_api = is_public_api
         self.user_id = user_id
         self.project = project
@@ -38,14 +46,6 @@ class RequestContext(context.RequestContext):
         self.auth_url = auth_url
         self.auth_token_info = auth_token_info
         self.trust_id = trust_id
-
-        super(RequestContext, self).__init__(auth_token=auth_token,
-                                             user=user, tenant=project,
-                                             is_admin=is_admin,
-                                             read_only=read_only,
-                                             show_deleted=show_deleted,
-                                             request_id=request_id,
-                                             roles=roles)
 
     def to_dict(self):
         return {'auth_token': self.auth_token,
