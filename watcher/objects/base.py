f54aca70cc287826897115f8f23565cf9ac3b6cf
Vincent Françoise <Vincent.FRANCOISE@b-com.com>
2016-09-23 16:46:41 +0200

    Added 'goal' ObjectField for Strategy object
    In this changeset, I added the "goal" ObjectField which can
    either be loaded by setting the new "eager" parameter as True or
    not loaded (as before) by setting it to False.
    The advantage of introducing this eager parameter is that this way,
    we can reduce to a minimum the overhead of DB queries whenever the
    related goal is not actually needed.
    
    Partially-Implements: blueprint watcher-versioned-objects
    Change-Id: I103c9ed161d2cedf7b43c55f9e095ef66bf44dea

--- watcher/objects/base.py
+++ watcher/objects/base.py
@@ -103,6 +103,19 @@ class WatcherPersistentObject(object):
         'deleted_at': ovo_fields.DateTimeField(nullable=True),
     }
 
+    # Mapping between the object field name and a 2-tuple pair composed of
+    # its object type (e.g. objects.RelatedObject) and the the name of the
+    # model field related ID (or UUID) foreign key field.
+    # e.g.:
+    #
+    # fields = {
+    #     # [...]
+    #     'related_object_id': fields.IntegerField(),  # Foreign key
+    #     'related_object': wfields.ObjectField('RelatedObject'),
+    # }
+    # {'related_object': (objects.RelatedObject, 'related_object_id')}
+    object_fields = {}
+
     def obj_refresh(self, loaded_object):
         """Applies updates for objects that inherit from base.WatcherObject.
 
@@ -116,17 +129,39 @@ class WatcherPersistentObject(object):
                 self[field] = loaded_object[field]
 
     @staticmethod
-    def _from_db_object(obj, db_object):
+    def _from_db_object(obj, db_object, eager=False):
         """Converts a database entity to a formal object.
 
         :param obj: An object of the class.
         :param db_object: A DB model of the object
+        :param eager: Enable the loading of object fields (Default: False)
         :return: The object of the class with the database entity added
-
         """
+        obj_class = type(obj)
+        object_fields = obj_class.object_fields
 
         for field in obj.fields:
-            obj[field] = db_object[field]
+            if field not in object_fields:
+                obj[field] = db_object[field]
+
+        if eager:
+            # Load object fields
+            context = obj._context
+            loadable_fields = (
+                (obj_field, related_obj_cls, rel_id)
+                for obj_field, (related_obj_cls, rel_id)
+                in object_fields.items()
+                if obj[rel_id]
+            )
+            for obj_field, related_obj_cls, rel_id in loadable_fields:
+                if db_object.get(obj_field) and obj[rel_id]:
+                    # The object field data was eagerly loaded alongside
+                    # the main object data
+                    obj[obj_field] = related_obj_cls._from_db_object(
+                        related_obj_cls(context), db_object[obj_field])
+                else:
+                    # The object field data wasn't loaded yet
+                    obj[obj_field] = related_obj_cls.get(context, obj[rel_id])
 
         obj.obj_reset_changes()
         return obj
