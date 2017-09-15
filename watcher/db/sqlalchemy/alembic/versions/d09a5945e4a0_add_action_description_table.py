a5e7fd90c23ee7ea534ecfe8ea33c49ad590fa70
Alexander Chadin <a.chadin@servionica.ru>
2017-09-15 15:30:33 +0300

    Fix Watcher DB schema creation
    This patch set replaces create_schema with upgrade to fix
    apscheduler creation issue. It also fixes pep8 warnings to
    d09a5945e4a0_add_action_description_table.py
    
    Change-Id: Ica842d585ee3a9cd67e45eb1d7bb1916573d7c9c

--- watcher/db/sqlalchemy/alembic/versions/d09a5945e4a0_add_action_description_table.py
+++ watcher/db/sqlalchemy/alembic/versions/d09a5945e4a0_add_action_description_table.py
@@ -6,25 +6,29 @@ Create Date: 2017-07-13 20:33:01.473711
 
 """
 
-# revision identifiers, used by Alembic.
-revision = 'd09a5945e4a0'
-down_revision = 'd098df6021e2'
-
 from alembic import op
 import oslo_db
 import sqlalchemy as sa
 
+# revision identifiers, used by Alembic.
+revision = 'd09a5945e4a0'
+down_revision = 'd098df6021e2'
+
+
 def upgrade():
-    op.create_table('action_descriptions',
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('deleted_at', sa.DateTime(), nullable=True),
-    sa.Column('deleted', oslo_db.sqlalchemy.types.SoftDeleteInteger(), nullable=True),
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('action_type', sa.String(length=255), nullable=False),
-    sa.Column('description', sa.String(length=255), nullable=False),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('action_type', name='uniq_action_description0action_type')
+    op.create_table(
+        'action_descriptions',
+        sa.Column('created_at', sa.DateTime(), nullable=True),
+        sa.Column('updated_at', sa.DateTime(), nullable=True),
+        sa.Column('deleted_at', sa.DateTime(), nullable=True),
+        sa.Column('deleted', oslo_db.sqlalchemy.types.SoftDeleteInteger(),
+                  nullable=True),
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('action_type', sa.String(length=255), nullable=False),
+        sa.Column('description', sa.String(length=255), nullable=False),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('action_type',
+                            name='uniq_action_description0action_type')
     )
 
 
