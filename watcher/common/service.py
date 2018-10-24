cbaf38519ebfe008ec61aaf8ffa9bb91d195fdd9
licanwei <li.canwei2@zte.com.cn>
2018-10-24 16:07:09 +0800

    Watcher doesn't need paramiko
    Change-Id: I46a1df82b2b7e4051f1a844ad17d1afa758a1fcb

--- watcher/common/service.py
+++ watcher/common/service.py
@@ -68,7 +68,7 @@ _DEFAULT_LOG_LEVELS = ['amqp=WARN', 'amqplib=WARN', 'qpid.messaging=INFO',
                        'oslo.messaging=INFO', 'sqlalchemy=WARN',
                        'keystoneclient=INFO', 'stevedore=INFO',
                        'eventlet.wsgi.server=WARN', 'iso8601=WARN',
-                       'paramiko=WARN', 'requests=WARN', 'neutronclient=WARN',
+                       'requests=WARN', 'neutronclient=WARN',
                        'glanceclient=WARN', 'watcher.openstack.common=WARN',
                        'apscheduler=WARN']
 
